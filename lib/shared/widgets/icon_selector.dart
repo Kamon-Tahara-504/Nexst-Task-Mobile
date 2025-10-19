d405eed86f97fa65342aa71c0a80dea168e85d23
Kqn0n <taharacomeon.0504@gmail.com>
2025-10-19 12:00:06 +0900

    アイコン選択ウィジェットの追加

---
+++ lib/shared/widgets/icon_selector.dart
@@ -0,0 +1,330 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../core/constants/app_colors.dart';
+import '../../core/constants/app_sizes.dart';
+import 'liquid_glass_container.dart';
+
+/// 利用可能なアイコンのリスト（Web版から取得）
+const List<String> availableIcons = [
+  'angular',
+  'aws',
+  'bootstrap',
+  'cpp',
+  'csharp',
+  'css3',
+  'dart',
+  'django',
+  'docker',
+  'express',
+  'figma',
+  'flutter',
+  'git',
+  'github',
+  'go',
+  'html5',
+  'java',
+  'javascript',
+  'jest',
+  'kotlin',
+  'mongodb',
+  'mysql',
+  'nextjs',
+  'nodejs',
+  'npm',
+  'nuxtjs',
+  'php',
+  'postgresql',
+  'python',
+  'react',
+  'redux',
+  'ruby',
+  'rust',
+  'sass',
+  'spring',
+  'swift',
+  'tailwindcss',
+  'typescript',
+  'vite',
+  'vscode',
+  'vue',
+  'webpack',
+  'yarn',
+];
+
+/// アイコン選択Widget
+class IconSelector extends StatelessWidget {
+  /// 現在選択されているアイコン
+  final String? selectedIcon;
+
+  /// アイコン選択時のコールバック
+  final ValueChanged<String?> onIconSelected;
+
+  /// グリッドの列数
+  final int crossAxisCount;
+
+  const IconSelector({
+    super.key,
+    this.selectedIcon,
+    required this.onIconSelected,
+    this.crossAxisCount = 6,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        crossAxisSpacing: AppSizes.spaceSm,
+        mainAxisSpacing: AppSizes.spaceSm,
+        childAspectRatio: 1,
+      ),
+      itemCount: availableIcons.length + 1, // +1 for "None" option
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          // "アイコンなし" オプション
+          return _IconItem(
+            iconName: null,
+            isSelected: selectedIcon == null,
+            onTap: () => onIconSelected(null),
+          );
+        }
+
+        final iconName = availableIcons[index - 1];
+        return _IconItem(
+          iconName: iconName,
+          isSelected: selectedIcon == iconName,
+          onTap: () => onIconSelected(iconName),
+        );
+      },
+    );
+  }
+}
+
+/// アイコン項目Widget
+class _IconItem extends StatelessWidget {
+  final String? iconName;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const _IconItem({
+    this.iconName,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: isSelected
+              ? AppColors.primary.withOpacity(0.2)
+              : AppColors.glassBackground,
+          borderRadius: BorderRadius.circular(AppSizes.radiusSm),
+          border: Border.all(
+            color: isSelected ? AppColors.primary : AppColors.glassBorder,
+            width: isSelected ? 2.0 : 1.0,
+          ),
+        ),
+        padding: const EdgeInsets.all(AppSizes.paddingSm),
+        child: iconName != null
+            ? SvgPicture.asset(
+                'assets/icons/$iconName.svg',
+                width: AppSizes.icon,
+                height: AppSizes.icon,
+              )
+            : const Icon(
+                Icons.block,
+                size: AppSizes.icon,
+                color: AppColors.textDisabled,
+              ),
+      ),
+    );
+  }
+}
+
+/// アイコン選択モーダル
+class IconSelectorModal extends StatefulWidget {
+  /// 現在選択されているアイコン
+  final String? selectedIcon;
+
+  /// アイコン選択時のコールバック
+  final ValueChanged<String?> onIconSelected;
+
+  const IconSelectorModal({
+    super.key,
+    this.selectedIcon,
+    required this.onIconSelected,
+  });
+
+  @override
+  State<IconSelectorModal> createState() => _IconSelectorModalState();
+}
+
+class _IconSelectorModalState extends State<IconSelectorModal> {
+  String? _selectedIcon;
+  String _searchQuery = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIcon = widget.selectedIcon;
+  }
+
+  List<String> get _filteredIcons {
+    if (_searchQuery.isEmpty) {
+      return availableIcons;
+    }
+    return availableIcons
+        .where((icon) => icon.toLowerCase().contains(_searchQuery.toLowerCase()))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LiquidGlassContainer(
+      margin: const EdgeInsets.all(AppSizes.padding),
+      padding: const EdgeInsets.all(AppSizes.padding),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // ヘッダー
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'アイコンを選択',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: AppSizes.space),
+
+          // 検索フィールド
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'アイコンを検索...',
+              prefixIcon: Icon(Icons.search),
+            ),
+            onChanged: (value) {
+              setState(() {
+                _searchQuery = value;
+              });
+            },
+          ),
+
+          const SizedBox(height: AppSizes.space),
+
+          // アイコングリッド
+          Flexible(
+            child: SingleChildScrollView(
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 6,
+                  crossAxisSpacing: AppSizes.spaceSm,
+                  mainAxisSpacing: AppSizes.spaceSm,
+                  childAspectRatio: 1,
+                ),
+                itemCount: _filteredIcons.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return _IconItem(
+                      iconName: null,
+                      isSelected: _selectedIcon == null,
+                      onTap: () {
+                        setState(() {
+                          _selectedIcon = null;
+                        });
+                      },
+                    );
+                  }
+
+                  final iconName = _filteredIcons[index - 1];
+                  return _IconItem(
+                    iconName: iconName,
+                    isSelected: _selectedIcon == iconName,
+                    onTap: () {
+                      setState(() {
+                        _selectedIcon = iconName;
+                      });
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+
+          const SizedBox(height: AppSizes.space),
+
+          // アクションボタン
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('キャンセル'),
+              ),
+              const SizedBox(width: AppSizes.spaceSm),
+              ElevatedButton(
+                onPressed: () {
+                  widget.onIconSelected(_selectedIcon);
+                  Navigator.pop(context);
+                },
+                child: const Text('選択'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// アイコンプレビューWidget
+class IconPreview extends StatelessWidget {
+  /// アイコン名
+  final String? iconName;
+
+  /// サイズ
+  final double size;
+
+  const IconPreview({
+    super.key,
+    this.iconName,
+    this.size = AppSizes.iconLg,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (iconName == null || iconName!.isEmpty) {
+      return Icon(
+        Icons.code,
+        size: size,
+        color: AppColors.textDisabled,
+      );
+    }
+
+    return SvgPicture.asset(
+      'assets/icons/$iconName.svg',
+      width: size,
+      height: size,
+      placeholderBuilder: (context) => Icon(
+        Icons.image_not_supported,
+        size: size,
+        color: AppColors.textDisabled,
+      ),
+    );
+  }
+}
+
